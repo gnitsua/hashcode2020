9967951761084a95eb607afed22986726740cc53
augentry <augentry@amazon.com>
2020-02-20 11:38:50 -0800

    initial commit

---
+++ Library.py
@@ -0,0 +1,9 @@
+class Library:
+    def __init__(self, id, signup_time, shipping_rate, books = set()):
+        self.id = id
+        self.signup_time = signup_time
+        self.shipping_rate = shipping_rate
+        self.books = books
+
+    def __str__(self):
+        return "%s %s\n%s" % (self.id, len(self.books), " ".join(map(str,self.books)))
